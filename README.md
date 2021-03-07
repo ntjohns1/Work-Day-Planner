d18fe6e020c296f1c8ca5733da5a9f56fcbd9b7b
ntjohns1 <nelsontjohns@gmail.com>
2021-03-07 02:11:27 -0500

    Working on conditionalstatements for displaying line item classes based on current time. ToDo fix scope issue with currentTime.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Work-Day-Planner
+I began the project by using the setInterval method combined with moment.js to display the clock element at the top of the page that updates dynamically every second. 
+To create the hour block elements in the list, I created an array of business hours, and ran a for loop, using jQuery to create elements from the array.
--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,12 @@
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar*/
 
-var containerDiv = $('#main-container')
+var containerDiv = $("#main-container")
 // declare variable for moment js. and write it to #currentDay
 // use set interval method to dynamically update time every second
 setInterval(function () {
   var currentTime = moment();
-  $("#currentDay").text(currentTime.format("h:mm:ss MMM Do, YYYY"));
+  $("#currentDay").text(currentTime.format("h:mm:ss a MMM Do, YYYY"));
 }, 1000);
 
 
@@ -31,15 +31,16 @@ var times = [
 ]
 // for loop that cycles through the times and creates and attaches an element
 for (var i = 0; i < times.length; i++) {
-var timeList = $('<ul>');
-var hourBlock = $('<li>');
-var eventText = $('<textarea>')
-var saveBtn = $('<button>')
+var timeList = $("<ul>");
+var hourBlock = $("<li>");
+var eventText = $("<textarea>")
+var saveBtn = $("<button>")
 // classes for <ul>/<li> are from bootstrap; experiment with bootstrap for other elements
 timeList.addClass("list-group");
 containerDiv.append(timeList);
-// may need to add a class to style hourBlock.text
-hourBlock.text(times[i]);
+// may need to add a class to style hourBlock.text once we style the other elements
+// may need to assign hourBlock.text a variable so we can use it in conditional statements in next step
+var lineTitle = hourBlock.text(times[i]);
 hourBlock.addClass("list-group-item");
 // hourBlock.attribute("class",)
 // ToDo assign styles to text areas and save buttons
@@ -47,31 +48,34 @@ timeList.append(hourBlock);
 hourBlock.append(eventText);
 saveBtn.text("Save")
 hourBlock.append(saveBtn);
-
+console.log(lineTitle);
 }
 }
 renderBlocks(); 
 
 
 
-// these elements need a text area and a button element on the right when we send to local storage.
-// 
-
 /*WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future*/
-// create variables for the given classes
-// a series of conditionals will pass the designated classes to them based on the time. Maybe use math.floor to round to the hour?
-// 
+// set attributes with conditional statements
+// use timeCheck variable to compare value in lineTitle variable
+var timeCheck = currentTime.format("X");
+console.log(timeCheck);
+
+
+
 
 /*WHEN I click into a timeblock
 THEN I can enter an event*/
 // possibly create an event listener for the whole div so that user can click anywhere
-// make sure there is a text area, possibly a hidden form element inside the spans we create
+
+
 
 /*WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage*/
-// we need an event listener for the button on the right of the hour blocks
-// review target.event we are going to pass a that into a function to send the text area content to local storage
+// we need an event listener for saveBtn
+// review using target.event to grab the user input from eventText, there is probably a jQuery shortcut
+// We are going to pass a that into a function to send the user input text to local storage
 
 
 /*WHEN I refresh the page
