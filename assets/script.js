495e10edd5f5e25be7c7e3008532caaff2dc59cd
ntjohns1 <nelsontjohns@gmail.com>
2021-03-08 23:58:24 -0500

    working with local storage functionality

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ WHEN I open the planner
 THEN the current day is displayed at the top of the calendar*/
 
 var containerDiv = $("#main-container")
+var saveClick = console.log($("button"));
 // declare variable for moment js. and write it to #currentDay
 // use set interval method to dynamically update time every second
 setInterval(function () {
@@ -33,29 +34,34 @@ function renderBlocks() {
     // for loop that cycles through the times and creates and attaches an element
     for (var i = 0; i < times.length; i++) {
         var hourBlock = $("<li>");
-        var eventText = $("<textarea>")
+        var eventText = $("<input>")
         var saveBtn = $("<button>")
         // classes for <ul>/<li> are from bootstrap; experiment with bootstrap for other elements
         timeList.addClass("list-group");
         containerDiv.append(timeList);
         // may need to add a class to style hourBlock.text once we style the other elements
         // figure out how to get this value and pass it into logic on line 64+
+        // 
         var timeDisplay = hourBlock.text(times[i] > 12 ? `${times[i] - 12}:00` : `${times[i]}:00`);
-        hourBlock.attr("class", "time-block")
+        hourBlock.attr("class", "time-block");
         hourBlock.addClass("list-group-item");
-        // hourBlock.attribute("class",)
-        // ToDo assign styles to text areas and save buttons
         timeList.append(hourBlock);
-        eventText.attr("class", "textarea")
+        // WHEN I click into a timeblock, THEN I can enter an event
+        eventText.attr("type", "text");
+        eventText.attr("class", "textarea");
+        eventText.attr("name", "textarea");
+        eventText.attr("id", times[i] - 8);
+        // add something here to uniquely identify this one
         hourBlock.append(eventText);
         saveBtn.attr("class", "saveBtn");
         saveBtn.text("Save");
         hourBlock.append(saveBtn);
         var timeCheck = moment().hours();
-        
-        console.log(timeCheck);
-        console.log((times[i]));
-        
+        /*WHEN I view the timeblocks for that day
+        THEN each timeblock is color coded to indicate whether it is in the past, present, or future*/
+        // set class attributes with conditional statements
+        // use timeCheck variable to compare value in times[i]
+
         if (timeCheck > (times[i])) {
             hourBlock.attr("class", "past");
         }
@@ -67,30 +73,36 @@ function renderBlocks() {
         if (timeCheck === (times[i])) {
             hourBlock.attr("class", "present");
         }
+        saveBtn.on('click', function(event) {
+            event.preventDefault();
+            var line1 = $("#1")
+            var line2 = $("#2")
+            var line3 = $("#3")
+            var line4 = $("#4")
+            var line5 = $("#5")
+            var line6 = $("#6")
+            var line7 = $("#7")
+            var line8 = $("#8")
+            var line9 = $("#9")
 
+            console.log(line1.val());
+            localStorage.setItem("event", line1.val());
             
-
-            /*WHEN I view the timeblocks for that day
-            THEN each timeblock is color coded to indicate whether it is in the past, present, or future*/
-            // set attributes with conditional statements
-            // use timeCheck variable to compare value in lineTitle variable, can we use .isBefore, .isAfter, etc.?
-
-
+        });
     }
-}
+} 
 renderBlocks();
 
+    // Prevent the default behavior
+    
+    // get the text area that is associated with with this save button
+    // get the value in it
+    // do the save
 
 
 
 
-// if timeCheck is > time listed in hourBlock, attr("class", "past")
-// if timeCheck is < time listed in hourBlock, attr("class", "future")
-// if timeCheck is === then attr("class", "present")
 
-/*WHEN I click into a timeblock
-THEN I can enter an event*/
-// possibly create an event listener for the whole div so that user can click anywhere
 
 
 
